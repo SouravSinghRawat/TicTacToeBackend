f26bfb1c5719447f1b6a7ec5824a3f42423e1c41
SouravSinghRawat <souravprofessional20122001@gmail.com>
2026-03-08 13:57:32 +0530

    fx

--- src/main/java/game/tictactoe/controller/GameController.java
+++ src/main/java/game/tictactoe/controller/GameController.java
@@ -8,8 +8,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class GameController {
     private final GameService gameService;
     private final SimpMessagingTemplate messagingTemplate;
